fe81f095677980092a0dbfc8c6dcf710e4f1acc8
olgam <olgamartyniuk88@gmail.com>
2024-06-07 21:07:31 +0200

    setting up navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,38 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
- <h1>hello </h1>
+  <div class="container-fluid bg-primary border-bottom border-secondary border-4">
+    <nav class="navbar navbar-expand-lg bg-primary">
+      <div class="container-fluid">
+          <a class="navbar-brand text-white" href="#">FESTIVA</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link custom-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link custom-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link custom-link" href="#">Pricing</a>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+  </div>
+  <h1>hello</h1>
 </template>
+
+<style scoped lang="scss"></style>
